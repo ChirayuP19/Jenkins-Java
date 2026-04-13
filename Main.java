fc2cb8e0e92f2dbb2494a8d40d986866243720cd
ChirayuP19 <chirayu010675@gmail.com>
2026-04-13 01:43:16 -0400

    one more file added

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main (String[] args) {
         System.out.println("This is a Java program. and Jenkins is running this program.");
         System.out.println(java.time.LocalDateTime.now());
+        System.out.println(java.time.LocalDateTime.now().getHour());
     }
 }
