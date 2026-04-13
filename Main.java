5c586422f978b0758898df88ec8a09c985f2031e
ChirayuP19 <chirayu010675@gmail.com>
2026-04-13 01:28:58 -0400

    Jenkins and Maven Working

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("This is a Java program. and Jenkins is running this program.");
+        System.out.println(java.time.LocalDateTime.now());
+    }
+}
