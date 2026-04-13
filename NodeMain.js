78d51a82ca47402445142021d81b9e2c84e4ab21
ChirayuP19 <chirayu010675@gmail.com>
2026-04-13 02:02:56 -0400

    node added

--- NodeMain.js
+++ NodeMain.js
@@ -1,3 +1,5 @@
 console.log("This is a Node.js program. and Jenkins is running this program.");
 console.log(new Date());
 console.log(new Date().getHours());
+console.log("Student created: Chirayu, Age: 20");
+console.log("Student created: Kavya, Age: 22");
