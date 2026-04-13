cffb2c01d4e787ea5f5930ecd4cbfb79cf3e3392
ChirayuP19 <chirayu010675@gmail.com>
2026-04-13 01:54:06 -0400

    Student class added ..

--- Main.java
+++ Main.java
@@ -3,5 +3,8 @@ public class Main {
         System.out.println("This is a Java program. and Jenkins is running this program.");
         System.out.println(java.time.LocalDateTime.now());
         System.out.println(java.time.LocalDateTime.now().getHour());
+        Student student= new Student("Chirayu", 20);
+        System.out.println("Student created: " + student.getName() + ", Age: " + student.getAge());
+        student.newStudent("Kavya",22);
     }
 }
